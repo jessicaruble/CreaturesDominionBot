1b76b3fec18e1baf7d33128a1efb68814d1e6610
jessicaruble <103578892+jessicaruble@users.noreply.github.com>
2026-07-25 21:51:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ from keep_alive import keep_alive
 # 1. Automatically detect paths between Android and Render Cloud
 if os.path.exists('/storage/emulated/0/Download/CreaturesDominionBot'):
     PROJECT_DIR = '/storage/emulated/0/Download/CreaturesDominionBot'
+
 elif os.path.exists('/storage/emulated/0/CreaturesDominionBot'):
     PROJECT_DIR = '/storage/emulated/0/CreaturesDominionBot'
+
 else:
-    PROJECT_DIR = os.getcwd()
+    # Render / GitHub deployment
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if PROJECT_DIR not in sys.path:
+    sys.path.insert(0, PROJECT_DIR)
+
+print("PROJECT DIR:", PROJECT_DIR)
     sys.path.insert(0, PROJECT_DIR)
 
 # 2. Safe Database Import
